24560691721a663745d88f2e4555dbe735aa58cf
Carlos <thedarkpoketrainer@gmail.com>
2024-04-24 09:56:20 -0500

    Segunda prueba front

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ const to_do = [
     'Comprar entradas para el cine',
 ]
 
+const ul = GI.createElement('ul')
+document.body.appendChild(ul)
+to_do.forEach(item=>{
+    const li = GI.createElement('li')
+    const checkbox = GI.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.setAttribute('value', item)
+    
+    checkbox.addEventListener('change', ()=>{
+       if(checkbox.checked){
+        checkbox.classList.add('text-checked') 
+       } else checkbox.classList.remove('text-checked')
+    })
+    ul.appendChild(li)
+    ul.appendChild(checkbox)
+    li.innerHTML = li.innerHTML + item
+})
